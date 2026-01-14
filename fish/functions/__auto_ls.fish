64d51a18982447332f8d7f5139e06692f9849af4
nazozokc <nazozokc@gmail.com>
2026-01-14 20:30:44 +0900

    Add files via upload

---
+++ fish/functions/__auto_ls.fish
@@ -0,0 +1,12 @@
+# cd したら自動で ls
+function __auto_ls --on-variable PWD
+    # 端末幅が狭いと邪魔なので制限
+    if test (tput cols) -ge 60
+        echo
+        if command -q exa
+            exa -lh --icons
+        else
+            ls -lh
+        end
+    end
+end
