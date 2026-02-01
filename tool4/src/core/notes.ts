f6db8a874e3685ec0b0ceddf839bb313e49ae927
nazozokc <nazozokc@gmail.com>
2026-02-01 20:36:31 +0900

    add

--- tool4/src/core/notes.ts
+++ tool4/src/core/notes.ts
@@ -2,7 +2,8 @@ import { Note } from "../storage/file.js;
 
 export const addNote = (notes: Note[], text: string): Note[] = {
   const nextId = notes.length === 0 ? 1: notes[notes.length -1].id + 1;
-
-  r
 }
+
+
+
 }
