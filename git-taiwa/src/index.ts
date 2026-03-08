28134c34814e343213f80bcb8c4e3e7b4e62ef9e
nazozokc <nazozokc@gmail.com>
2026-03-08 14:19:23 +0900

    edit index.ts

--- git-taiwa/src/index.ts
+++ git-taiwa/src/index.ts
@@ -2,6 +2,16 @@ import { execa } from 'execa';
 import * as p from '@clack/prompts';
 import { Command } from 'commander';
 
-const program = new Command();
 
+const runCLI = () => {
+  const program = new Command;
 
+  program.name("git-taiwa").description("git taiwa tool").version("v1.0.0");
+
+  program
+    .command("gt")
+    .argument("git taiwa")
+    .action(async () => {
+
+    })
+}
