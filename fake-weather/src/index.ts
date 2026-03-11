79de5cd1c30556d32f0abe3ae82c23239de03ba2
nazozokc <nazozokc@gmail.com>
2026-03-11 15:34:19 +0900

    edit

--- fake-weather/src/index.ts
+++ fake-weather/src/index.ts
@@ -1,2 +1,21 @@
+#!/usr/bin/env node
 import { consola } from "consola";
-import { Command } from "commander";
+import { Command, CommanderError } from "commander";
+
+const runCLI = () => {
+  const program = new Command();
+
+  program.name("fake-weather").description("git taiwa tool").version("v1.0.0");
+
+  program
+    .command("tokyo")
+    .action(() => {
+      consola.log("Tokyo");
+      consola.log("weather: bag");
+      consola.log("time: UTC+9:00")
+    });
+
+  program.parse();
+}
+
+runCLI();
