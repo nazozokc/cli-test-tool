3da508d96cc5644a81a329abda1f3ffe5fa90d38
nazozokc <nazozokc@gmail.com>
2026-03-11 16:19:19 +0900

    edit .github/dependabot.yml

---
+++ time/src/index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import { consola } from "consola";
+import { getTime } from "./time.js";
+
+const runCLI = () => {
+  const program = new Command();
+
+  program.name("time").description("watch time tool").version("v1.0.0");
+
+  program
+    .command("tokyo")
+    .action(() => {
+      consola.log(`tokyo: ${getTime()}`);
+    });
+};
+
+runCLI();
