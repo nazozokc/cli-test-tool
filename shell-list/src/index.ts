98019735b61994321e26bda1fc95dd915abd5d7f
nazozokc <nazozokc@gmail.com>
2026-03-15 17:05:52 +0900

    edit: shell-list

--- shell-list/src/index.ts
+++ shell-list/src/index.ts
@@ -1,2 +1,17 @@
 import consola from "consola";
 import { Command } from "commander";
+import { readFileSync } from "fs";
+
+const shells = readFileSync("/etc/shells", "utf-8").split("\n").filter(l => l.startsWith("/"));
+
+const runCLI = () => {
+  const program = new Command();
+
+  program.name("shell").description("shell list view tool").version("1.0.0");
+
+  program
+    .command("view")
+    .action(async () => {
+      consola.log(shells);
+    })
+}
