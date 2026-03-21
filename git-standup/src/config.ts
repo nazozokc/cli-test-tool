429918f460a13ee25cf37ec2b6159d3f4cc68f54
nazozokc <nazozokc@gmail.com>
2026-03-21 14:56:48 +0900

    edit: src/

---
+++ git-standup/src/config.ts
@@ -0,0 +1,21 @@
+import os from "os";
+import path from "path";
+import fs from "fs";
+
+type Config = {
+  repos: string[];
+};
+
+export const getConfigPath = (): string => {
+  return path.join(os.homedir(), ".config", "git-standup", "repos.json");
+};
+
+export const loadConfig = (): Config => {
+  if (fs.existsSync(getConfigPath())) {
+    const content = fs.readFileSync(getConfigPath(), "utf-8");
+    const parsed = JSON.parse(content);
+    return parsed;
+  } else {
+    return { repos: [] };
+  }
+};
