5e0907b7e740f10a2d5acad4381b3ce54ae52d12
nazozokc <nazozokc@gmail.com>
2026-03-24 14:41:22 +0900

    edit build and add .gitignore

--- riaju-cli/src/index.ts
+++ riaju-cli/src/index.ts
@@ -5,7 +5,7 @@ const runCLI = () => {
   const program = new Command();
 
   program.name("riaju").version("v1.0.0").argument("<text>").argument("<text2>").action((text, text2) => {
-    consola.log(text + text2 + "は爆発してほしいが幸せになってほしいよーー、幸せになれーーーーーー！")
+    consola.log(text + "と" + text2 + "は爆発してほしいが幸せになってほしいよーー、幸せになれーーーーーー！")
   })
 
   program.parse();
