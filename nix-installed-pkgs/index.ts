5b41a3d19e9066936a37615883d075eb5d0f75e4
nazozokc <nazozokc@gmail.com>
2026-03-28 15:24:59 +0900

    nix

--- nix-installed-pkgs/index.ts
+++ nix-installed-pkgs/index.ts
@@ -1,11 +1,23 @@
 import { consola } from "consola";
 import { Command } from "commander";
-import { exec } from "exec";
+import { exec, execSync } from "child_process";
 
 const runCLI = () => {
   const program = new Command();
   program
     .name("nixlist")
     .description("nix installed package tool")
-    .version("1.0.0");
+    .version("1.0.0")
+    .action(async () => {
+      try {
+        const stdout = await execSync(`nix profile list` { encoding: `utf-8`});
+        consola.log(stdout)
+      } catch (error) {
+        consola.error("エラーが発生しました");
+      }
+    });
+
+    program.parse();
 };
+
+runCLI()
