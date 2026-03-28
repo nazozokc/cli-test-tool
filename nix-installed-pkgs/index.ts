dbd0b6bb2febc936718353d05d1c6a2c47a8fbb1
nazozokc <nazozokc@gmail.com>
2026-03-28 14:44:59 +0900

    nix

--- nix-installed-pkgs/index.ts
+++ nix-installed-pkgs/index.ts
@@ -2,3 +2,10 @@ import { consola } from "consola";
 import { Command } from "commander";
 import { exec } from "exec";
 
+const runCLI = () => {
+  const program = new Command();
+  program
+    .name("nixlist")
+    .description("nix installed package tool")
+    .version("1.0.0");
+};
