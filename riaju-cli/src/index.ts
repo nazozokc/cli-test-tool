dba9063dbf43e5e09f2c7f816324974c22ac782d
nazozokc <nazozokc@gmail.com>
2026-03-28 15:59:28 +0900

    nix

--- riaju-cli/src/index.ts
+++ riaju-cli/src/index.ts
@@ -4,11 +4,21 @@ import { consola } from "consola";
 const runCLI = () => {
   const program = new Command();
 
-  program.name("riaju").version("v1.0.0").argument("<text>").argument("<text2>").action((text, text2) => {
-    consola.log(text + "と" + text2 + "は爆発してほしいが幸せになってほしいよーー、幸せになれーーーーーー！")
-  })
+  program
+    .name("riaju")
+    .version("v1.0.0")
+    .argument("<text>")
+    .argument("<text2>")
+    .action((text: string, text2: string) => {
+      consola.log(
+        text +
+          "と" +
+          text2 +
+          "は爆発してほしいが幸せになってほしいよーー、幸せになれーーーーーー！",
+      );
+    });
 
   program.parse();
-}
+};
 
 runCLI();
