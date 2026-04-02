9a3d7ad4cd46f0de4d0286156db856da7c9c1208
nazozokc <nazozokc@gmail.com>
2026-04-02 19:13:35 +0900

    edit .gitignore

--- gat/index.ts
+++ gat/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env node
+import { consola } from "consola";
+import { Command } from "commander";
+import { readFileSync } from "fs";
+
+const runCLI = () => {
+  const program = new Command();
+
+  program
+    .name("gat")
+    .description("file view tool")
+    .argument("<files...>")
+    .action((files: string[]) => {
+      for (const file of files) {
+        const view = readFileSync(file, "utf-8");
+        consola.log(view);
+      }
+    });
+
+  program.parse();
+};
+
+runCLI();
+
