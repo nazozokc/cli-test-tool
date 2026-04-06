c11ff13cbc6210149f0fb3451c71b3e4d41bed21
nazozokc <nazozokc@gmail.com>
2026-04-06 12:14:12 +0900

    edit

--- sisou-cli/src/index.ts
+++ sisou-cli/src/index.ts
@@ -5,17 +5,22 @@ import { Command } from "commander";
 const runCLI = () => {
   const program = new Command();
 
-  program.name("sisou").description("思想家の発言をするためのCLI").version("v1.0.0")
-  .argument("<text>")
-  .action((text) => {
-    if (typeof text === "string") {
-      consola.log(text + "は存在するんだ！、〇〇主義こそ正義だ‐‐‐‐‐‐‐‐‐‐‐‐‐‐！")
-    } else {
-      consola.log("どうやら文字ではないな、しっかり文字を入力したまえ")
-    }
-  })
+  program
+    .name("sisou")
+    .description("思想家の発言をするためのCLI")
+    .version("v1.0.0")
+    .argument("<text>")
+    .action((text) => {
+      if (typeof text === "string") {
+        consola.log(
+          text + "は存在するんだ！、〇〇主義こそ正義だ‐‐‐‐‐‐‐‐‐‐‐‐‐‐！",
+        );
+      } else {
+        consola.log("どうやら文字ではないな、しっかり文字を入力したまえ");
+      }
+    });
 
-  program.parse()
-}
+  program.parse();
+};
 
 runCLI();
