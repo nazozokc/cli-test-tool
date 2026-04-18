ce6116da256d6004b7e0d984128a59ed2855cc7f
nazozokc <nazozokc@gmail.com>
2026-04-18 16:37:18 +0900

    edit

--- envcheck/index.ts
+++ envcheck/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { consola } from "consola";
+import { Command } from "commander";
+
+const runCLI = () => {
+  const program = new Command();
+
+  program.parse();
+};
+
+runCLI();
+
